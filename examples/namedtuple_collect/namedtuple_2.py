30e18d5e3e8c0d26d1308144bbbb655784c515c7
ArnoCan <acue_sf2@sourceforge.net>
2019-12-05 15:09:55 +0100

    update-00.01.013

--- examples/namedtuple_collect/namedtuple_2.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-
-from namedtuplex.namedtuple import namedtuple
-
-
-T = namedtuple('TestClass', ('a', 'b', 'c'), verbose=True, fielddefaults=(('c', 33), ('b', 22)))
-
-t = T(11, 22, 33)
-print(t)
